2d640634f1ba026118d925c3a831ee8561da48a9
Simone <simone.magnani4@studio.unibo.it>
2022-01-07 17:22:16 +0100

    Add generic utility

---
+++ src/utility.lua
@@ -0,0 +1,35 @@
+MAX_VELOCITY = 20           --Sets the max speed of the robot
+
+--gap must be positive
+function move_index_of(start, gap, max)
+  assert(gap >= 0)
+  new_index = (start + gap) % max
+  if new_index == 0 then
+    return max
+  else
+    return new_index
+  end
+end
+
+--Choose and turn to the shortest way
+function turn(target, actual, half)
+  -- Decide the shortest way
+  if (target > actual and target - half < actual)
+   or (target < actual and target + half < actual) then
+    turn_counter_clock()
+  else
+    turn_clock()
+  end
+end
+
+function turn_clock()
+  robot.wheels.set_velocity(MAX_VELOCITY,-MAX_VELOCITY)
+end
+
+function turn_counter_clock()
+  robot.wheels.set_velocity(-MAX_VELOCITY,MAX_VELOCITY)
+end
+
+function go_ahead()
+  robot.wheels.set_velocity(MAX_VELOCITY,MAX_VELOCITY)
+end
