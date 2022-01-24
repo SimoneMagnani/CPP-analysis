bc41093f6252c635e9c6fc0b7652d53f83a7c331
Simone <simone.magnani4@studio.unibo.it>
2022-01-24 13:52:41 +0100

    add state utilities

--- src/cell_state.lua
+++ src/cell_state.lua
@@ -11,20 +11,32 @@ CELL_STATE = {
 function init_cell_state(matrix, set_state)
   for i=1, #matrix do
     for j=1, #matrix[i] do
-      matrix[i][j].state = set_state(i, j) -- set_cell_state?
+      set_cell_state_from_coord(matrix, i, j, set_state(i, j))
     end
   end
 end
 
 function get_cell_state(matrix, cell)
   assert(cell, "cell nil")
-  if not coords_are_valid(matrix, cell.i, cell.j) then return CELL_STATE.UNVISITABLE end
-  return matrix[cell.i][cell.j].state
+  if not cell then return CELL_STATE.UNVISITABLE end
+  return get_cell_state_from_coord(matrix, cell.i, cell.j)
+end
+
+function get_cell_state_from_coord(matrix, i, j)
+  if not coords_are_valid(matrix, i, j) then return CELL_STATE.UNVISITABLE end
+  return matrix[i][j].state
 end
 
 function set_cell_state(matrix, cell, new_state)
-  matrix[cell.i][cell.j].state = new_state
-  --log(cell_to_string(cell), " is ", state_to_string(new_state))
+  if cell then
+    set_cell_state_from_coord(matrix, cell.i, cell.j, new_state)
+  end
+end
+
+function set_cell_state_from_coord(matrix, i, j, new_state)
+  if coords_are_valid(matrix, i, j) then
+    matrix[i][j].state = new_state
+  end
 end
 
 function state_to_string(state)
